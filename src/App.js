5b78ed21b7cea8edea762ec29a4a1f07b50b1670
LeyGittyHub <lovelycasandig@gmail.com>
2024-02-18 23:46:36 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import SideNav from './components/SideNav'
+
+function App() {
+  return (
+    <div>
+      <SideNav />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/SideNav.js
@@ -0,0 +1,72 @@
+import Logo from '../assets/github.png'
+import {useState} from 'react';
+import { IoIosArrowForward } from "react-icons/io"
+import { RiStarSmileFill } from "react-icons/ri";
+import { TbDiamondFilled } from "react-icons/tb";
+import { BsSuitHeartFill } from "react-icons/bs";
+import { ImFolderUpload } from "react-icons/im";
+
+const SideNav = () => {
+  const [open, setOpen] = useState(true)
+  
+
+  return ( 
+
+<div className='flex'>
+
+  <div 
+      className={` duration-500 h-screen p-5 pt-8 bg-[#172554] ${open ? "w-72" : "w-24"} 
+      duration-500 relative`}>
+  <IoIosArrowForward 
+      className={` absolute cursor-pointer rounded-full -right-3 top-9 bg-white text-3xl border-2 border-y-indigo-600 
+      ${!open && "rotate-180"}`}
+        onClick={() => setOpen(!open)}
+  />
+
+<div className='inline-flex'>
+  <img className='cursor-pointer absolute top-0 left-4 mt-4 duration-500 w-14' src={Logo} alt="Github Logo"
+      />
+
+  <h1 
+    className={`text-white font-medium origin-left ml-20 text-xl duration-500 ${!open && "scale-0"}`}>FLAR-J
+      </h1>
+
+  <ul>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+  </ul>
+
+
+
+  
+</div>
+
+
+</div>
+<div className='p-7'>
+  <h1 className='text-2xl font-semibold'>PLAY ME!</h1>
+</div>
+
+</div>     
+
+
+
+  )
+}
+
+export default SideNav
+
+
+
+
+{/*<ul className='pt-24 text-white font-bold '>
+          <li className='p-[16%]'>PLAY ME!</li>
+          <li className='p-[16%]'>READ ME!</li>
+          <li className='p-[16%]'>KNOW ME!</li>
+          <li className='p-[16%]'>UPLOAD</li>
+              </ul>
+            
+            ${!open && "rotate-180"}*/}
